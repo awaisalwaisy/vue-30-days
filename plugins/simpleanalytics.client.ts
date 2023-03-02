fcb20710d8b42286eef404fb720f45bd3aff0b6b
Awais al-waisy <awaisalwaisy@gmail.com>
2023-03-02 07:12:42 +0500

    simple analytics plugin url fix

--- plugins/simpleanalytics.client.ts
+++ plugins/simpleanalytics.client.ts
@@ -3,6 +3,6 @@ import SimpleAnalytics from "simple-analytics-vue";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(SimpleAnalytics, {
     skip: import.meta.env.NODE_ENV !== "production",
-    domain: "https://vue30days.netlify.app/",
+    domain: "vue30days.netlify.app",
   });
 });
