3fbcb81a3aae5185ecd75b7eaed06fabe7f28a13
Awais al-waisy <awaisalwaisy@gmail.com>
2023-03-02 07:03:27 +0500

    simple analytics added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
   extends: "@nuxt-themes/docus",
   modules: ["@nuxtjs/supabase", "@nuxthq/ui", "@pinia/nuxt"],
+  plugins: [{ src: "~/plugins/simple-analytics.ts", ssr: false }],
 });
