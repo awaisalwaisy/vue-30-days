22b2b2226348961079fb6b5677f7824f295c9889
Awais al-waisy <awaisalwaisy@gmail.com>
2023-03-02 10:16:45 +0500

    meta cover bug fixed

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,8 @@ export default defineAppConfig({
     title: "Vue 30 Days",
     description:
       "The best place to learn VueJs in 30 lessons. Every lesson is a challenge to help you learn VueJs in a fun way.",
-    image: "./assets/vue30days cover.png",
+    image:
+      "https://raw.githubusercontent.com/awaisalwaisy/vue-30-days/master/assets/vue30days%20cover.png",
     socials: {
       twitter: "alvaisy",
       github: "awaisalwaisy",
