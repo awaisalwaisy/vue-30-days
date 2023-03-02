0d4a4243e29eb1a6cf211e70f53d67c4f62a313a
Awais al-waisy <awaisalwaisy@gmail.com>
2023-03-02 10:03:39 +0500

    meta cover added

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,9 @@
 export default defineAppConfig({
   docus: {
     title: "Vue 30 Days",
-    description: "The best place to learn VueJs in 30 lessons.",
-    image:
-      "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
+    description:
+      "The best place to learn VueJs in 30 lessons. Every lesson is a challenge to help you learn VueJs in a fun way.",
+    image: "./assets/vue30days cover.png",
     socials: {
       twitter: "alvaisy",
       github: "awaisalwaisy",
